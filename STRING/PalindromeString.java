e0ca4cbb13515d1e6965f1f399a8b69029a5f6da
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2024-10-02 20:51:14 +0530

    Add files via upload

---
+++ STRING/PalindromeString.java
@@ -0,0 +1,23 @@
+// Given a string S, check if it is palindrome or not.
+
+public class PalindromeString {
+    public boolean isPalindrome(String s) {
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
+        }
+        return s.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        PalindromeString obj1 = new PalindromeString();
+        PalindromeString obj2 = new PalindromeString();
+        String s1 = "madam";
+        String s2 = "hello";
+        System.out.println(obj1.isPalindrome(s1));
+        System.out.println(obj2.isPalindrome(s2));
+    }
+}
+
+// The time complexity of this code is O(n) because of iteration used to get the
+// reverse word.
